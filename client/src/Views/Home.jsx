fccc00706c6ff2cd2e06d0810d915069d3e8fb4d
Aqil Thanawala <aqil.thanawala@gmail.com>
2020-01-24 17:25:54 -0800

    Add dependencies and get frontend to render using webpack dev server

--- client/src/Views/Home.jsx
+++ client/src/Views/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Hero from "../components/Hero";
-import Content from "../components/Content";
+import Hero from "../Components/Hero";
+import Content from "../Components/Content";
 
 const Home = () => (
   <>
