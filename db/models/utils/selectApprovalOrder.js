e33ecb925b4a9a07f8cd80d098da9f48056b1eea
Aqil Thanawala <aqil.thanawala@gmail.com>
2020-03-29 16:06:15 -0700

    Create demo function for adding approval orders in live environment

--- db/models/utils/selectApprovalOrder.js
+++ db/models/utils/selectApprovalOrder.js
@@ -759,6 +759,60 @@ const costCenters = {
   },
 };
 
+const demoCostCenters = {
+  routes: {
+    approverOne: [
+      {
+        email: 'aqil@pmcoc.com',
+        isApproved: false,
+        isSent: false,
+        dateSent: null,
+        dateApproved: null,
+      },
+    ],
+    approverTwo: [
+      {
+        email: 'aqil@pmcoc.com',
+        isApproved: false,
+        isSent: false,
+        dateSent: null,
+        dateApproved: null,
+      },
+      {
+        email: 'aqil@peoplescali.com',
+        isApproved: false,
+        isSent: false,
+        dateSent: null,
+        dateApproved: null,
+      },
+    ],
+    approverThree: [
+      {
+        email: 'aqil@pmcoc.com',
+        isApproved: false,
+        isSent: false,
+        dateSent: null,
+        dateApproved: null,
+      },
+      {
+        email: 'aqil@peoplescali.com',
+        isApproved: false,
+        isSent: false,
+        dateSent: null,
+        dateApproved: null,
+      },
+      {
+        email: 'aqilt@mac.com',
+        isApproved: false,
+        isSent: false,
+        dateSent: null,
+        dateApproved: null,
+      },
+    ],
+  }
+}
+
+
 function selectApprovalOrder(context) {
   let approvalRoute = 'approverOne';
   const { invoiceTotal, costCenter } = context;
@@ -767,6 +821,20 @@ function selectApprovalOrder(context) {
   if(invoiceTotal > 2500) approvalRoute = 'approverThree'
 
   return costCenters[costCenter].routes[approvalRoute] || [{ email: 'Aqil@pmcoc.com', isApproved: false, isSent: false }]
+}
+
+//  changing the costCenter to the demo one specifically to test
+//  in a live environment without bugging the company.
+
+function demoSelectApprovalOrder(context) {
+  let approvalRoute = 'approverOne';
+  const { invoiceTotal } = context;
+
+  if(invoiceTotal > 1000) approvalRoute = 'approverTwo'
+  if(invoiceTotal > 2500) approvalRoute = 'approverThree'
+
+  return demoCostCenters.routes[approvalRoute] || [{ email: 'Aqil.thanawala@gmail.com', isApproved: false, isSent: false }]
+
 }
 
 module.exports = selectApprovalOrder;
