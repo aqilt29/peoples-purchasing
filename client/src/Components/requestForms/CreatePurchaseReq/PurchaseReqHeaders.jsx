d2841ca9ee627c44430f13e53dbede4d8224b19c
Aqil Thanawala <aqil.thanawala@gmail.com>
2020-04-19 19:38:15 -0700

    Can now enter in custom payment terms

--- client/src/Components/requestForms/CreatePurchaseReq/PurchaseReqHeaders.jsx
+++ client/src/Components/requestForms/CreatePurchaseReq/PurchaseReqHeaders.jsx
@@ -4,7 +4,7 @@ import { AvForm, AvField, AvGroup, AvInput } from 'availity-reactstrap-validatio
 import { BlueButton } from '../../../Styles';
 import VendorSelect from '../../VendorSelect';
 import UserSelect from '../../UserSelect';
-import { listOfBuyers, listOfPaymentTerms, listOfShippingAddresses } from '../../../utils/lists';
+import { listOfPaymentTerms, listOfShippingAddresses } from '../../../utils/lists';
 import EntitySelect from '../../EntitySelect';
 import { useAuth0 } from '../../../react-auth0-spa';
 
@@ -13,20 +13,33 @@ export const PurchaseReqHeaders = ({ setHeaders, requestToEdit }) => {
   const [entity, setEntity] = useState(requestToEdit ? requestToEdit.entity._id : null)
   const [user, setUser] = useState(requestToEdit ? requestToEdit.user._id : null)
   const [buyer, setBuyer] = useState(requestToEdit ? requestToEdit.buyer._id : null)
+
+  const [customTerms, setCustomTerms] = useState(null)
+  const [isOtherTerms, setIsOtherTerms] = useState(false)
+
   const [isValid, setValid] = useState(false)
   const { dbUser: { _id: currentUserId } } = useAuth0()
 
   console.log(currentUserId)
   console.log(entity, vendor, user, '<- from request to edit')
 
-  const submitValidHeaders = (_, formData) => {
-    console.log(entity, vendor, user, buyer)
+  const submitValidHeaders = (_, { paymentTerms, customTerms, shipTo, businessNeed, isBlanket}) => {
+    console.log(entity, vendor, user, buyer, paymentTerms, customTerms)
+    setCustomTerms(customTerms)
+    if (paymentTerms === 'Other' && customTerms.length < 1) {
+      window.alert('Please enter Payment Terms');
+      return;
+    };
+
     const reqHeaders = {
       vendor: vendor.value || requestToEdit.vendor._id,
       buyer: buyer.value || requestToEdit.buyer._id,
       entity: entity.value || requestToEdit.entity._id,
       submittedFor: user.value || requestToEdit.submittedFor._id,
-      ...formData
+      paymentTerms: paymentTerms === 'Other' ? customTerms : paymentTerms,
+      shipTo,
+      businessNeed,
+      isBlanket
     }
     console.log(reqHeaders);
 
@@ -49,24 +62,13 @@ export const PurchaseReqHeaders = ({ setHeaders, requestToEdit }) => {
             <UserSelect userId={requestToEdit && (requestToEdit.user._id !== currentUserId) ? requestToEdit.user._id : undefined} label="Request on behalf of:" userChange={setUser} />
             <EntitySelect entityId={requestToEdit ? requestToEdit.entity._id : undefined} entityChange={setEntity} />
             <UserSelect userId={requestToEdit && (requestToEdit.buyer._id !== currentUserId) ? requestToEdit.user._id : undefined} label="Person placing the order:" userChange={setBuyer} />
-            {/* <AvField
-              defaultValue={requestToEdit ? requestToEdit.buyer : undefined}
-              style={{ width: '75%' }}
-              type="select"
-              name="buyer"
-              label="Employee Placing the Order:"
-            >
-              <option value="">Select A User...</option>
-              {
-                listOfBuyers.map(({ name, value }, idx) => <option key={idx} value={value}>{name}</option>)
-              }
-            </AvField> */}
           </Col>
           <Col>
-              <AvField
+            <AvField
               defaultValue={requestToEdit ? requestToEdit.paymentTerms : undefined}
               type="select"
               required
+              onChange={(_, value) => value === 'Other' ? setIsOtherTerms(true) : setIsOtherTerms(false) }
               name="paymentTerms"
               label="Payment Terms:"
               validate={{required: {value: true, errorMessage: 'Please select an option from the list'}}}
@@ -76,6 +78,15 @@ export const PurchaseReqHeaders = ({ setHeaders, requestToEdit }) => {
                 listOfPaymentTerms.map((term, idx) => <option key={idx} value={term}>{term}</option>)
               }
             </AvField>
+            <AvField
+              labelHidden={ isOtherTerms ? false : true }
+              style={{ visibility: isOtherTerms ? 'visible' : 'hidden' }}
+              defaultValue={requestToEdit ? requestToEdit.paymentTerms : undefined}
+              name="customTerms"
+              label="Custom Payment Terms:"
+              type="text"
+              placeholder="Enter Custom Terms if Applicable"
+            />
             <AvField
               defaultValue={requestToEdit ? requestToEdit.address.shipTo : undefined}
               type="select"
