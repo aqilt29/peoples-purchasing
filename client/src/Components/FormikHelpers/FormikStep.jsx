30f696e5c4cb7d1552901731b978eb124fad983d
Aqil Thanawala <aqil.thanawala@gmail.com>
2020-06-16 16:39:54 -0700

    stub out components needed for multiform setup, add notes in the comments

---
+++ client/src/Components/FormikHelpers/FormikStep.jsx
@@ -0,0 +1,6 @@
+/**
+ * This component will hold Formik's field components
+ * This component will also hold the validation schema for
+ * the FormikStepper
+ */
+
---
+++ client/src/Components/FormikHelpers/FormikStepper.jsx
@@ -0,0 +1,7 @@
+/**
+ * This component serves as a wrapper around field content
+ * The goal is to have variable steps that have their own validation
+ * but the stepper is what holds the one formik component
+ * that way there arent multiple formiks and combining their
+ * outputs doesnt become crazy complex
+ */
