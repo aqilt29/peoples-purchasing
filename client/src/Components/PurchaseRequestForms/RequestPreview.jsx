2d0c281b6e5fba2553fd9981759faf117c99e9a7
Aqil Thanawala <aqil.thanawala@gmail.com>
2020-06-19 11:54:39 -0700

    Request preview details populate a little less than ideal but it works for now, will refine. TODO: connect to API add DATES FOR THE ITEMS

--- client/src/Components/PurchaseRequestForms/RequestPreview.jsx
+++ client/src/Components/PurchaseRequestForms/RequestPreview.jsx
@@ -9,20 +9,71 @@ const RequestPreview = () => {
     <>
       <Container>
         <Row>
-          <Col>
-            <h3>Request Preview</h3>
-          </Col>
+          <Col><h3>Request Preview</h3></Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col md={3}><h6>Reference/Project Name:</h6></Col>
+          <Col md={3}><p>{values.referenceName}</p></Col>
+          <Col md={3}><h6>Purchasing Entity:</h6></Col>
+          <Col md={3}><p>{values.entity.label}</p></Col>
         </Row>
         <Row>
-        <Col>
-          <h3>Request Items List</h3>
-          <ItemsCart items={values.items} details />
-        </Col>
+          <Col md={3}><h6>Business Need/ Details:</h6></Col>
+          <Col md={3}><p>{values.businessNeed}</p></Col>
+          <Col md={3}><h6>Request Procurement by Company:</h6></Col>
+          <Col md={3}><p>{values.needBuyer ? 'Yes' : 'No'}</p></Col>
+          <Col md={3}><h6>Invoice Total:</h6></Col>
+          <Col md={3}><p>{values.needBuyer ? 'Yes' : 'No'}</p></Col>
+          <Col md={6}>
+            <h6>Shipping Address:</h6>
+            <Row>
+              <Col>
+                Address:
+              </Col>
+              <Col>
+                {values.shippingAddress.address}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Address 2:
+              </Col>
+              <Col>
+                {values.shippingAddress.address2 || ''}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                City:
+              </Col>
+              <Col>
+                {values.shippingAddress.city}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                State:
+              </Col>
+              <Col>
+                {values.shippingAddress.state}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Zip:
+              </Col>
+              <Col>
+                {values.shippingAddress.zipCode}
+              </Col>
+            </Row>
+          </Col>
         </Row>
         <hr />
         <Row>
         <Col>
-          <pre>{JSON.stringify(values, null, 2)}</pre>
+          <h3>Request Items List</h3>
+          <ItemsCart items={values.items} details />
         </Col>
         </Row>
       </Container>
