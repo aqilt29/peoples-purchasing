696ff923e48bd30957ca34913071f9d57f3fc653
Aqil Thanawala <aqil.thanawala@gmail.com>
2020-06-19 02:09:21 -0700

    rework the item schema for itls

--- db/models/item.js
+++ db/models/item.js
@@ -1,41 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const units = [
-  'EA',
-  'LB',
-  'GM',
-  'MG',
-  'KG',
-  'ML',
-  'L',
-  'BOX',
-  'CS',
-  'PK',
-  'HR',
-  'BT',
-  'FT',
-  'RL',
-  'GL',
-  'M',
-  'DZ',
-]
-
 const itemSchema = new Schema({
   description: { type: String, required: true },
+  specialDetails: { type: String, required: true },
+  expenseCategory: { type: String, required: true },
   price: { type: Number, required: true },
   requestByDate: { type: String, required: true },
   unitOfMeasure: { type: String, default: 'ea' },
   link: String,
   quantity: { type: Number, required: true },
-  vendorItemNumber: String,
-  vendorPartNumber: String,
-  internalPartNumber: String,
-  materialGroup: String,
-  ledger: String,
-  classCode: String, //  needs ENUM
-
-  isDirect: { type: Boolean, default: false },
   receivedQty: { type: Number, default: 0 },
   invoicedQty: { type: Number, default: 0 },
 });
