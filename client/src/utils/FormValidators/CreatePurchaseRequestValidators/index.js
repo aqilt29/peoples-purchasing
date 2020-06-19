40e7581aa84d3ec14579035b1b5eb5d69a669be6
Aqil Thanawala <aqil.thanawala@gmail.com>
2020-06-18 23:16:19 -0700

    build out schema validation for headers TODO: build header field

--- client/src/utils/FormValidators/CreatePurchaseRequestValidators/index.js
+++ client/src/utils/FormValidators/CreatePurchaseRequestValidators/index.js
@@ -2,7 +2,14 @@ import * as yup from 'yup';
 
 export const headerValidators = yup.object()
   .shape({
-    referenceName: yup
-      .string()
-      .required('Required!'),
+    referenceName: yup.string().required('Required!'),
+    entity: yup.string().required('Required!'),
+    businessNeed: yup.string().required('Required!'),
+    shippingAddress: yup.object({
+      address: yup.string().required('Required!'),
+      address2: yup.string().required('Required!'),
+      city: yup.string().required('Required!'),
+      state: yup.string().required('Required!'),
+      zipCode: yup.string().required('Required!'),
+    }),
   })
